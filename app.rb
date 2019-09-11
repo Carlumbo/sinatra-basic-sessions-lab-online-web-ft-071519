d10ffa0e4fa39bbdf973c921a8f345808e367e97
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-09-11 04:43:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,14 @@ class App < Sinatra::Base
   
   configure do 
     enable :session 
-    set session_secret
+    set session_secret , 'batman'
   end 
+  
+  get '/' do 
+    erb :index
+  end 
+  
+  post '/checkout' do 
+  end 
+  
 end
